7baebd3d752f04f5d8f9e42ef88ff4ad8414822e
Daniel Bradley <daniel@pulumi.com>
2022-03-30 17:35:14 +0100

    Implement create script

--- index.ts
+++ index.ts
@@ -1,3 +1,223 @@
 #!/usr/bin/env node
 
-console.log("TODO: init project");
+import * as readline from "readline";
+import * as fs from "fs";
+import * as path from "path";
+
+const prompt = readline.createInterface(process.stdin, process.stdout);
+
+const readme = `
+# An Example Provider in Typescript
+
+## Prerequisites
+
+- Nodejs
+- Yarn
+
+Run \`yarn\` after checkout to restore packages. NPM can also be used, if preferred.
+
+## Debugging locally
+
+1. \`yarn start\` - starts the plugin
+2. In a Pulumi program directory, run \`PULUMI_DEBUG_PROVIDER=ts-random:[PORT] pulumi up\` to use your local development provider.
+
+Tip: In VSCode, run \`yarn start\` in the "JavaScript Debug Terminal" to automatically attach an enable breakpoints.
+
+## Building
+
+- \`yarn build\` - builds the package
+- \`yarn install-plugin\` - builds and installs the provider locally
+
+## Releasing
+
+### Setup
+
+Run \`yarn init-actions\` to create the recommended GitHub release workflow.
+
+### Process
+
+Push a tag in the format \`vx.x.x\`. This will build and create the release containing the asset.
+
+Edit the release notes after the workflow has completed if desired.
+`;
+
+const indexTemplate = `
+import * as pulumi from "@pulumi/pulumi";
+import * as schema from "./schema.json";
+
+pulumi.provider.main(
+  {
+    version: require("package.json").version ?? "0.0.0",
+
+    create: async (urn, inputs) => {
+      const urnParts = urn.split("::").reverse();
+      const id = urnParts[0];
+      const type = urnParts[1];
+      console.log("Create", { id, type });
+      const outs = { result: inputs.input };
+      return { id, outs };
+    },
+
+    update: async (id, urn, olds, news) => {
+      console.log("Update", { id });
+      const outs = { result: news.input };
+      return { outs };
+    },
+
+    delete: async (id, urn, props) => {
+      console.log("Delete", id);
+    },
+
+    schema: JSON.stringify(schema),
+  },
+  process.argv.slice(2)
+);
+`;
+
+const gitignore = `
+### macOS ###
+# General
+.DS_Store
+.AppleDouble
+.LSOverride
+
+# Thumbnails
+._*
+
+### Node ###
+# Logs
+logs
+*.log
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+lerna-debug.log*
+.pnpm-debug.log*
+.yarn-integrity
+.npm
+node_modules/
+*.tgz
+dist
+.yarn/cache
+.yarn/unplugged
+.yarn/build-state.yml
+.yarn/install-state.gz
+.pnp.*
+
+### Windows ###
+# Windows thumbnail cache files
+Thumbs.db
+Thumbs.db:encryptable
+ehthumbs.db
+ehthumbs_vista.db
+# Folder config file
+[Dd]esktop.ini
+# Windows shortcuts
+*.lnk
+`;
+(async function () {
+  try {
+    const name = await new Promise<string>((resolve) =>
+      prompt.question(
+        "Provider name? This will be used for your folder and package name\n> ",
+        resolve
+      )
+    );
+    if (!name.match(/^[a-z]([a-z]|-)*[a-z]$/)) {
+      console.error(
+        "Invalid provider name - must be lower case and hyphenated e.g. my-provider"
+      );
+      process.exit(1);
+    }
+    const packageContent = {
+      name,
+      dependencies: {
+        "@pulumi/pulumi": "^3.27.0",
+        "@supercharge/strings": "^1.25.0",
+      },
+      devDependencies: {
+        "create-pulumi-provider":
+          "https://github.com/danielrbradley/create-pulumi-provider",
+        "ts-node": "^10.7.0",
+        typescript: "^4.6.3",
+      },
+      scripts: {
+        start: "ts-node index.ts",
+        build: "pulumi-provider-scripts build",
+        "install-plugin": "pulumi-provider-scripts install",
+        "init-actions": "pulumi-provider-scripts init-actions",
+      },
+    };
+    const schema = {
+      $schema:
+        "https://raw.githubusercontent.com/pulumi/pulumi/master/pkg/codegen/schema/pulumi.json",
+      name,
+      license: "Apache-2.0",
+      provider: {
+        description: "Example provider",
+      },
+      resources: {
+        [`${name}:index:ExampleResource`]: {
+          description: "Example to demonstrate inputs and outputs",
+          properties: {
+            result: {
+              type: "string",
+              description: "Example string output property",
+            },
+          },
+          required: ["result"],
+          inputProperties: {
+            input: {
+              type: "string",
+              description: "Example string input property",
+            },
+          },
+          requiredInputs: ["input"],
+        },
+      },
+    };
+    const tsconfig = {
+      compilerOptions: {
+        target: "ES2017",
+        module: "commonjs",
+        declaration: true,
+        sourceMap: true,
+        stripInternal: true,
+        experimentalDecorators: true,
+        noFallthroughCasesInSwitch: true,
+        forceConsistentCasingInFileNames: true,
+        strict: true,
+        resolveJsonModule: true,
+        noEmit: true,
+      },
+    };
+
+    if (fs.existsSync(name)) {
+      if (fs.readdirSync(name).length !== 0) {
+        console.error("Directory is not empty, aborting");
+        process.exit(1);
+      }
+    } else {
+      fs.mkdirSync(name);
+    }
+
+    const writeString = (filePath: string, template: string) =>
+      fs.writeFileSync(path.join(name, filePath), template.trimStart());
+    const writeJson = (filePath: string, data: any) =>
+      writeString(filePath, JSON.stringify(data, undefined, 2));
+
+    writeJson("package.json", packageContent);
+    writeJson("tsconfig.json", tsconfig);
+    writeJson("schema.json", schema);
+    writeString("PulumiPlugin.yaml", "runtime: nodejs\n");
+    writeString("index.ts", indexTemplate);
+    writeString(".gitignore", gitignore);
+    writeString("README.md", readme);
+    console.log("Done. Read the README.md for next instructions");
+  } catch (e) {
+    console.error(e);
+    process.exitCode = 1;
+  } finally {
+    prompt.close();
+  }
+})();
